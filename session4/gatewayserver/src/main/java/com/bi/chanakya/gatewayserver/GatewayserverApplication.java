40cca8872b674b7bd6e489ca0379b5c985d7d429
kishorevbhosale <kishorevbhosale@gmail.com>
2022-08-23 00:26:22 +0530

    Custom routing using spring cloud gateway

--- session4/gatewayserver/src/main/java/com/bi/chanakya/gatewayserver/GatewayserverApplication.java
+++ session4/gatewayserver/src/main/java/com/bi/chanakya/gatewayserver/GatewayserverApplication.java
@@ -2,7 +2,12 @@ package com.bi.chanakya.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -10,6 +15,26 @@ public class GatewayserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p 
+						.path("/skbank/accounts/**")
+						.filters(f -> f.rewritePath("/skbank/accounts/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time",new Date().toString()))
+						.uri("lb://ACCOUNTS")).
+				route(p -> p
+						.path("/skbank/loans/**")
+						.filters(f -> f.rewritePath("/skbank/loans/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time",new Date().toString()))
+						.uri("lb://LOANS")).
+				route(p -> p
+						.path("/skbank/cards/**")
+						.filters(f -> f.rewritePath("/skbank/cards/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time",new Date().toString()))
+						.uri("lb://CARDS")).build();
 	}
 
 }
